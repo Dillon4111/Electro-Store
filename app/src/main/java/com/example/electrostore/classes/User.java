2c4d1474268f71649761f78a1441e37cd5764026
Dillon Rochford <dillonrochford@gmail.com>
2021-03-15 16:16:28 +0000

    Initial commit

---
+++ app/src/main/java/com/example/electrostore/classes/User.java
@@ -0,0 +1,37 @@
+package com.example.electrostore.classes;
+
+public class User {
+    private String uid, name, address;
+
+    public User() {
+    }
+
+    public User(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
